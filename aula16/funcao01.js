bb590ce1c03c4efb48d56e8c2f82baa2883472d7
Vicente Marçal <vicente.marcal@gmail.com>
2019-11-15 19:58:38 -0400

    Notas da aula 16

---
+++ aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimpa(n) {
+    if (n % 2 == 0){
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+let res = parimpa(4);
+
+console.log(res)
+
+console.log(parimpa(223))
