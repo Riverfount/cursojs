e7098c99ce5cc0d89811bf9e2a37414fe559e861
Vicente Marçal <vicente.marcal@gmail.com>
2019-11-16 11:24:12 -0400

    Refatorado exercício 18

--- aula16ex/ex018/assets/js/script.js
+++ aula16ex/ex018/assets/js/script.js
@@ -3,15 +3,11 @@ let myNumbers = document.querySelector('select#myNumbers');
 let resp = document.querySelector('div#resp');
 let listagem = [];
 
-function isInInterval(n) {
-    return Number(n) >= 1 && Number(n) <= 100;
-}
+let isInInterval = n => Number(n) >= 1 && Number(n) <= 100;
 
-function isInArray(num, list) {
-    return list.indexOf(Number(num)) !== -1;
-}
+let isInArray = (num, list)=> list.indexOf(Number(num)) !== -1;
 
-function adicionar() {
+let adicionar = ()=> {
     if (isInInterval(num.value) && !isInArray(num.value, listagem)){
         listagem.push(Number(num.value));
         let item = document.createElement('option');
@@ -23,31 +19,23 @@ function adicionar() {
     }
     num.value = '';
     num.focus();
-}
+};
 
-function somaArray(arr) {
-    let result = 0;
-    for (let element in arr) {
-        result += arr[element]
-    }
-    return result
-}
-
-function finalizar() {
+let finalizar = ()=> {
     if (listagem.length != 0) {
-        listagem.sort(function (a, b){return a-b});
+        listagem.sort((a, b)=>a-b);
         console.log(listagem);
         let qde_lista = listagem.length;
         let max = listagem[qde_lista - 1];
         let min = listagem[0];
-        let soma = somaArray(listagem);
+        let soma = listagem.reduce((a, b)=>a+b);
         let media = soma / qde_lista;
         resp.innerHTML =  `<p>Ao todo, temos ${qde_lista} números cadastrados</p>`;
         resp.innerHTML += `<p>O maior valor informado foi ${max}</p>`;
         resp.innerHTML += `<p>O menor valor informado foi ${min}</p>`;
         resp.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`;
-        resp.innerHTML += `<p>A média dos valores digitados é: ${media}</p>`
+        resp.innerHTML += `<p>A média dos valores digitados é: ${media.toFixed(2).replace('.', ',')}</p>`
     } else {
         alert('Adicione valores antes de finalizar!')
     }
-}
+};
